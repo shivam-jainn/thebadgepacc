2df8b137c61a1ae6404201ebf5ac64face932e43
shivam-jainn <shivam.clgstash@gmail.com>
2023-12-31 13:09:12 +0530

    try2

--- app/(post-signin)/[username]/page.tsx
+++ app/(post-signin)/[username]/page.tsx
@@ -23,7 +23,12 @@ const BadgesPage = () => {
   const [tokens, setTokens] = useState<any[]>([]);
   const [badgeImages, setBadgeImages] = useState<BadgeImage[]>([]);
   const [selectedBadgeIndex, setSelectedBadgeIndex] = useState<number | null>(null);
-
+  
+  const handleModalOpen = (index: number) => {
+    setSelectedBadgeIndex(index);
+    onOpen();
+  };
+  
   useEffect(() => {
     const fetchUserData = async () => {
       try {
@@ -33,7 +38,6 @@ const BadgesPage = () => {
           setTokens(data.tokens);
           setBadgeImages(data.badgeImages);
           console.log(data);
-
         } else {
           throw new Error('Failed to fetch user data');
         }
@@ -45,85 +49,83 @@ const BadgesPage = () => {
     fetchUserData();
   }, []);
 
-  const handleModalOpen = (index: number) => {
-    setSelectedBadgeIndex(index);
-    onOpen();
-  };
-
   const { data: session, status } = useSession();
   console.log(session);
-
 
   return (
     <>
       {status === 'loading' || !session || !session.user ? (
+        <div>
           <Skeleton>
             <Card className='flex flex-row justify-between max-w-sm gap-12 p-4 m-auto'>
-              <Avatar src={session?.user.image} size='lg' radius='sm' isBordered />
+              <Avatar src={session?.user?.image} size='lg' radius='sm' isBordered />
               <div className='flex flex-col'>
-                <h2>{session?.user.name}</h2>
-                <p>{`@${session.user.username}`}</p>
+                <h2>{session?.user?.name}</h2>
+                <p>{`@${session?.user?.username}`}</p>
               </div>
             </Card>
           </Skeleton>
+        </div>
       ) : (
-      <>
-        <div>
-          <Card className='flex flex-row justify-between max-w-sm gap-12 p-4 m-auto'>
-            <Avatar src={session.user.image} size='lg' radius='sm' isBordered />
-            <div className='flex flex-col'>
-              <h2>{session.user.name}</h2>
-              <p>{`@${session.user.username}`}</p>
-            </div>
-          </Card>
-        </div>
-
-        <Divider className='max-w-lg m-auto my-4 bg-gray-500' />
-
-        <div className='max-w-lg grid-cols-3 sm:grid-cols-1'>
-          {tokens.map((token, index) => (
-            <Card
-              isFooterBlurred
-              radius="lg"
-              className="max-w-[200px] p-4 m-auto border-none"
-              key={index}
-            >
-              {badgeImages[index] ? (
-                <img
-                  alt="Badge"
-                  className="object-cover m-auto mb-4 rounded-lg"
-                  height={200}
-                  src={badgeImages[index].pic}
-                  width={200}
-                  onClick={() => handleModalOpen(index)}
-                />
-              ) : (
-                <p>Loading...</p>
-              )}
-              <Button onClick={() => handleModalOpen(index)}>View More</Button>
+        <>
+          <div>
+            <Card className='flex flex-row justify-between max-w-sm gap-12 p-4 m-auto'>
+              <Avatar src={session.user.image} size='lg' radius='sm' isBordered />
+              <div className='flex flex-col'>
+                <h2>{session.user.name}</h2>
+                <p>{`@${session.user.username}`}</p>
+              </div>
             </Card>
-          ))}
-        </div>
+          </div>
 
-        <Modal isOpen={isOpen} onClose={onClose} backdrop="blur">
-          <ModalContent>
-            <ModalHeader>Badge Details</ModalHeader>
-            <ModalBody>
-              {selectedBadgeIndex !== null && (
-                <p>Details for badge {selectedBadgeIndex} will be shown here.</p>
-              )}
-            </ModalBody>
-            <ModalFooter>
-              <Button color="danger" onClick={onClose}>
-                Close
-              </Button>
-              <Button color="primary" onClick={onClose}>
-                Action
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
+          <Divider className='max-w-lg m-auto my-4 bg-gray-500' />
+
+          <div className='max-w-lg grid-cols-3 sm:grid-cols-1'>
+            {tokens && (
+              tokens.map((token, index) => (
+                <Card
+                  isFooterBlurred
+                  radius="lg"
+                  className="max-w-[200px] p-4 m-auto border-none"
+                  key={index}
+                >
+                  {badgeImages[index] ? (
+                    <img
+                      alt="Badge"
+                      className="object-cover m-auto mb-4 rounded-lg"
+                      height={200}
+                      src={badgeImages[index].pic}
+                      width={200}
+                      onClick={() => handleModalOpen(index)}
+                    />
+                  ) : (
+                    <p>Loading...</p>
+                  )}
+                  <Button onClick={() => handleModalOpen(index)}>View More</Button>
+                </Card>
+              ))
+            )}
+          </div>
+
+          <Modal isOpen={isOpen} onClose={onClose} backdrop="blur">
+            <ModalContent>
+              <ModalHeader>Badge Details</ModalHeader>
+              <ModalBody>
+                {selectedBadgeIndex !== null && (
+                  <p>Details for badge {selectedBadgeIndex} will be shown here.</p>
+                )}
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" onClick={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onClick={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </>
       )}
     </>
   );
